7b7a210298083488347fbea1726a56941152a104
Natalia Koroleva <natalia.koroleva@shapelets.io>
2021-12-22 11:53:48 +0100

    add aggregated bar chart with tooltips

--- basic-bar-chart/app.js
+++ basic-bar-chart/app.js
@@ -1,30 +1,34 @@
-const horizontalRadioButton = document.querySelector('#horizontal');
-const verticalRadioButton = document.querySelector('#vertical');
-const radioButtonsGroup = document.getElementsByName('bar-chart-layout');
+const horizontalRadioButton = document.querySelector("#horizontal");
+const verticalRadioButton = document.querySelector("#vertical");
+const complexRadioButton = document.querySelector("#complex");
+const radioButtonsGroup = document.getElementsByName("bar-chart-layout");
 
 const callChart = (specJson) => {
-    vegaEmbed("#vis", specJson)
+  vegaEmbed("#vis", specJson)
     // result.view provides access to the Vega View API
-    .then(result => console.log(result))
+    .then((result) => console.log(result))
     .catch(console.warn);
-}
+};
 callChart("vertical-bar.vg.json");
 
 const findChecked = () => {
-    let spec;
-    for (let i = 0; i < radioButtonsGroup.length; i++) {
-        if (radioButtonsGroup[i].checked) {
-            console.log(radioButtonsGroup[i].value);
-            if (radioButtonsGroup[i].value === "vertical") {
-                spec = "vertical-bar.vg.json";
-            }
-            else if (radioButtonsGroup[i].value === "horizontal") {
-                spec = "horizontal-bar.vg.json";
-            }
-        }
+  let spec;
+  for (let i = 0; i < radioButtonsGroup.length; i++) {
+    if (radioButtonsGroup[i].checked) {
+      console.log(radioButtonsGroup[i].value);
+      if (radioButtonsGroup[i].value === "vertical") {
+        spec = "vertical-bar.vg.json";
+      } else if (radioButtonsGroup[i].value === "horizontal") {
+        spec = "horizontal-bar.vg.json";
+      } else if (radioButtonsGroup[i].value === "complex") {
+        spec = "complex-bar-chart.vg.json";
+      }
     }
-    callChart(spec);
-}
+  }
+  console.log(spec.data);
+  callChart(spec);
+};
 
-horizontalRadioButton.addEventListener('click', event => findChecked());
-verticalRadioButton.addEventListener('click', event => findChecked());
+horizontalRadioButton.addEventListener("click", () => findChecked());
+verticalRadioButton.addEventListener("click", () => findChecked());
+complexRadioButton.addEventListener("click", () => findChecked());
