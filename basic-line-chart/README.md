3d036a473ecdf12bba6bd97ab95c69803d7e8e08
nk <natalia.koroleva@shapelets.io>
2021-12-30 14:11:47 +0100

    add multiple line chart done in Vega

---
+++ basic-line-chart/README.md
@@ -0,0 +1,1 @@
+Line chart in Vega forked from [here](https://vega.github.io/vega/examples/line-chart/) with a population data from [vega datasets](https://vega.github.io/vega-datasets/)
--- basic-line-chart/app.js
+++ basic-line-chart/app.js
@@ -5,3 +5,13 @@ const callChart = (specJson) => {
     .catch(console.warn);
 };
 callChart("line-chart.vg.json");
+
+const view = new vega.View(runtime, {
+  logLevel: vega.Warn, // view logging level
+  renderer: "svg", // render type (defaults to 'canvas')
+  container: "#vis", // parent DOM element
+  hover: true, // enable hover event processing
+});
+view._runtime;
+
+view.runAsync(); // evaluate and render the view
